6569cb92e96b7a47f6940be8f73fb4c9c2473bb3
Jakub Szczepański <jakub.teodor.szczepanski@gmail.com>
2018-12-17 14:01:28 +0100

    JS update
    Added preventDefault, so sounds are played after truning on MPD by clicking start button.

--- out.js
+++ out.js
@@ -1,22 +1,5 @@
-function removeTransition(e) {
-    if (e.propertyName !== 'transform') return;
-    e.target.classList.remove('playing');
-}
-
-function playSound(e) {
-    const audio = document.querySelector(`audio[data-pad="${e.keyCode}"]`);
-    const pad = document.querySelector(`div[data-pad="${e.keyCode}"]`);
-    if (!audio) return;
-
-    pad.classList.add('playing');
-    audio.currentTime = 0;
-    audio.play();
-}
-
 const pads = Array.from(document.querySelectorAll('.pad'));
-
 pads.forEach(pad => pad.addEventListener('transitionend', removeTransition));
-
 window.addEventListener('keydown', playSound);
 
 function startMPD(e) {
@@ -30,3 +13,21 @@ function startMPD(e) {
 }
 
 startMPD();
+
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-pad="${e.keyCode}"]`);
+    const pad = document.querySelector(`div[data-pad="${e.keyCode}"]`);
+    if (!audio) return;
+    if (pad.classList.contains('activated')) {
+        pad.classList.add('playing');
+        audio.currentTime = 0;
+        audio.play();
+    } else {
+        e.preventDefault;
+    }
+}
+
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing');
+}
